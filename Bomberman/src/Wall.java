f8e5fa46b059ea49295858c93c25fcc491cef0f9
Giulia Kirstein <giulia.kirstein@uni-duesseldorf.de>
2012-05-28 20:58:19 +0200

    RENA MÖHLMANN WALL
    Erste Klasse Wall(Breakable, Power) von Rena Möhlmann, da ihr GitHub
    wieder mal nicht will…

---
+++ Bomberman/src/Wall.java
@@ -0,0 +1,67 @@
+
+/**
+ * @author Rena Moehlmann (2042105)
+ *
+ */
+public class Wall {
+
+	private boolean breakable;
+	private int positionX, positionY; //nicht zwingend notwendig?
+	private int power; //Anzahl der Male, die eine Wand von einer Bombe getroffen werden muss, um kaputt zu gehen
+
+	
+	public Wall(boolean breakable) {
+		this.breakable = breakable;
+		//beliebige Position der Wand
+		randomStrange();
+	}
+	
+	public Wall(boolean breakable, int positionX, int positionY) {
+		this.breakable = breakable;
+		this.positionX = positionX;
+		this.positionY = positionY;
+		//bestimmte Position der Wand
+		randomStrange();
+	}
+	
+	
+	public Wall(int positionX, int positionY) {
+		this.positionX = positionX;
+		this.positionY = positionY;
+		this.breakable = false;
+		//Konstruktor für feste Wand
+	}
+
+	private void randomStrange() {
+		int randomNumber = (int) (Math.random()*10);
+		if (randomNumber < 4)
+			this.power = 1;
+		if (randomNumber > 3 && randomNumber < 7)
+			this.power = 2;
+		if (randomNumber > 6 && randomNumber <= 10)
+			this.power = 3;
+		//bestimmt Anzahl der Male, die eine Wand von einer Bombe getroffen werden muss, um kaputt zu gehen
+	}
+	
+	public boolean isBreakable() {
+		return this.breakable;
+		//gibt Wert der Wand zurück
+	}
+
+	public void subPower() {
+		power--;
+	}
+	
+	public int getPower() {
+		return this.power;
+	}
+
+	public int getPositionX() {
+		return this.positionX;
+	}
+
+	public int getPositionY() {
+		return this.positionY;
+	}
+	
+}
